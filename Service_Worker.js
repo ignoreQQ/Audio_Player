e9c75bb6e46264e40a6d2aacf3735c4c29a01169
ignoreQQ <ignoreqq123@gmail.com>
2026-04-12 21:13:14 +0800

    fix note

--- Service_Worker.js
+++ Service_Worker.js
@@ -1,12 +1,10 @@
-const CACHE_NAME = 'lyric-player-v3'; // 每次大更新就改一下這個名字 (v1 -> v2)
+const CACHE_NAME = 'lyric-player-v3';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json'
-  // 之後可以把 icon 也加進來
 ];
 
-// 安裝時強制跳過等待，讓新版 Service Worker 立即生效
 self.addEventListener('install', (event) => {
   self.skipWaiting(); 
   event.waitUntil(
@@ -14,7 +12,6 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// 關鍵！啟動時自動刪除舊版的快取檔案
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
